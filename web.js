8c05b706d39fd9e639011c50d532259f8d7f27f0
Ubuntu <ubuntu@ip-10-164-9-235.ec2.internal>
2013-07-04 17:20:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,13 +5,28 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	
  // response.send('Hello World 2! Hola !!');
+  //--------------------
   
-  //read index.html contents
-  
-  buf = new Buffer(256);
-buf =  fs.readFileSync("index.html");
-console.log(buf.toString());
-response.send(buf.toString());
+  var fs = require("fs");
+var fileName = "index.html";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          console.log(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+
+//---------------
+   
+response.send(data);
   
 });
 
