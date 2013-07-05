7e89b4f5a53fde2aa8b57fd2941baaba848e24a6
Ubuntu <ubuntu@ip-10-164-9-235.ec2.internal>
2013-07-05 21:15:03 +0000

    updated for HW3

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -26,41 +27,97 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler');
 
-var assertFileExists = function(infile) {
+
+var downloadFile = function (url) {
+    rest.get(url).on('complete', function (result) {
+        if (result instanceof Error) {
+            sys.puts('Error: ' + result.message);
+            this.retry(5000); // try again after 5 sec
+        } else {
+            // sys.puts(result);
+            //sys.puts("file downloaded");
+            fs.writeFile("./tintin.html", result, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    //console.log("The file was saved!");
+                }
+            });
+        }
+    });
+
+}
+
+
+
+
+var assertFileExists = function (infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if (!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
+
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+
+
+var cheerioHtmlFile = function (htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
+
+
+
+var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
-if(require.main == module) {
+
+function list(val) {
+    return val.split(',');
+}
+
+
+
+
+
+if (require.main == module) {
+
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+   		 //.option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+   		 .option('-u, --url <url>', 'URL to remote file')
+    		.parse(process.argv);
+
+    //process.argv.forEach(function(val, index, array) {
+    //	console.log(index + ': ' + val);
+    //});
+
+    //console.log(' Args: %j', program.args);
+
+    //console.log('checks : %s' , program.checks.toString());
+    // console.log('URL :%s' , program.url);
+
+    //  download file locally and save it
+
+    downloadFile(program.url);
+    var checkJson = checkHtmlFile("./tintin.html", program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
